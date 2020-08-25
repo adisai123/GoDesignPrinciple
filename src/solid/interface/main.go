0e77b17af0ab23812819d3eec517bc95809dbd50
Ram <adisai123@gmail.com>
2020-08-25 23:19:20 +0530

    interfaceseperator

--- src/solid/interface/main.go
+++ src/solid/interface/main.go
@@ -1,7 +1,60 @@
 package main
 
+import "fmt"
+
 //interface segregation
 //old fasion and latest (scan , print, fax seperate seprate interface instead of only one printer interface)
-func main() {
 
+type basicprinter interface {
+	print()
+}
+
+type basicFax interface {
+	fax()
+}
+type basicScanner interface {
+	scan()
+}
+
+type multifunctionPrinter interface {
+	basicprinter
+	basicScanner
+	basicFax
+}
+
+type oldPrinter struct {
+	msg string
+}
+
+func (oldp *oldPrinter) print() {
+	fmt.Println("msg to print :=>", oldp.msg)
+}
+
+type mordenprinter struct {
+	doc string
+}
+
+func (mp *mordenprinter) print() {
+	fmt.Println("print document :=", mp.doc)
+}
+
+func (mp *mordenprinter) scan() {
+	fmt.Println("scanning document :=", mp.doc)
+}
+
+func (mp *mordenprinter) fax() {
+	fmt.Println("srnding fax of this  document :=", mp.doc)
+}
+
+func usePrinter(p multifunctionPrinter) {
+	p.scan()
+	p.print()
+	p.fax()
+}
+
+func main() {
+	oldP := oldPrinter{"printing on old printer"}
+	oldP.print()
+	modernP := mordenprinter{"aadhar"}
+	usePrinter(&modernP)
 }
